c9c70aa9d60f5033b43b261308652ff10cd7cd92
Stephan Osterburg <stephanosterburg@gmail.com>
2020-11-07 10:12:43 -0800

    removed build and debug folder

--- build/thirdparty/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/stephanosterburg/Projects/cpprouteplanner/thirdparty/googletest/googlemock
-# Build directory: /Users/stephanosterburg/Projects/cpprouteplanner/build/thirdparty/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
